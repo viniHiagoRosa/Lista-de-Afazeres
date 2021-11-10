e1fe30c1fbf3ab8d7b1dfce9274e3a1abcc922f3
viniHiagoRosa <93688147+viniHiagoRosa@users.noreply.github.com>
2021-11-10 15:13:36 -0300

    atualização do JS

--- scrpit.js
+++ scrpit.js
@@ -1,11 +1,14 @@
 const btn = document.querySelector('#btn-incluir');
 
 btn.addEventListener('click', function(e){
-
+    
     const inclusao = document.querySelector('#inclusao')
 
     const value = inclusao.value;
 
+    var btn = document.createElement('li');
+    btn.innerHTML = value;
+    document.getElementById('incluir').appendChild(btn);
     
-
+   
 })
