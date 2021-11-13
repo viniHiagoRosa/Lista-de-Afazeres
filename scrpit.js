abb0cc7a93f974f6adb505bdc8a5edbed1b11c92
viniHiagoRosa <93688147+viniHiagoRosa@users.noreply.github.com>
2021-11-12 21:59:07 -0300

    Estlização e Organização do JS

--- scrpit.js
+++ scrpit.js
@@ -1,14 +1,76 @@
 const btn = document.querySelector('#btn-incluir');
 
-btn.addEventListener('click', function(e){
-    
+btn.addEventListener('click', function(){
+
     const inclusao = document.querySelector('#inclusao')
-
     const value = inclusao.value;
-
-    var btn = document.createElement('li');
-    btn.innerHTML = value;
-    document.getElementById('incluir').appendChild(btn);
     
+    var local_storage = localStorage.getItem('item')
+
+    var _local_storage = localStorage.getItem('tarefas');
+    var arrayTarefas = new Array();
+    var json_local_storage 
+    
+    if(_local_storage=== null){
+        var json = '{"tarefas": ["' + inclusao.value + '"]}';
+        var objeto = JSON.parse(json);
+        
+    
+        var json_local_storage = JSON.stringify(objeto);
+        localStorage.setItem('tarefas', json_local_storage);
+
+    }else{
+        arrayTarefas = JSON.parse(_local_storage);
+        arrayTarefas['tarefas'].push(inclusao.value);
+
+        json_local_storage = JSON.stringify(arrayTarefas);
+        localStorage.setItem('tarefas', json_local_storage)
+    }
    
+    let checkbox = document.createElement('input');
+    let li = document.createElement('li');  
+    let btn = document.createElement('button');  
+        
+    checkbox.id = 'checkbox'
+    checkbox.setAttribute('type', 'checkbox')    
+    li.id = 'lista'    
+    btn.id = 'lista'  
+    btn.innerHTML = 'Deletar';
+    btn.onclick = function() { excluir(this) };
+    
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode(inclusao.value));
+    li.appendChild(btn)
+    document.querySelector('ul').appendChild(li)
+
+    inclusao.value = "";
+
+    excluir()
+    
 })
+
+function excluir(elemento){
+    _local_storage = localStorage.getItem('tarefas');
+    var objeto = JSON.parse(_local_storage)
+    arrayTarefas = objeto.tarefas;
+
+    var novo_array = arrayTarefas.filter(valor => valor !== elemento.id)
+
+    objeto.tarefas = novo_array;
+
+    _local_storage = JSON.stringify(objeto);
+    localStorage.setItem('tarefas', _local_storage)
+
+    confirm('Você tem certeza?')
+
+    if(confirm === false){
+        
+    }
+
+    document.getElementById('lista').remove();
+}
+
+
+
+
+    
